8c82ac64d5229132fd9a0c6a5a2334923de9b183
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-08 17:17:18 +0300

    add: create the ContactStorage class

---
+++ src/main/java/com/pulsar/ContactStorage.java
@@ -0,0 +1,18 @@
+package com.pulsar;
+
+import com.pulsar.model.Contact;
+
+import java.util.*;
+
+public class ContactStorage {
+
+    private final List<Contact> orderedContacts;
+    private final Set<Contact> contacts;
+    private final Map<String, Contact> groupedContacts;
+
+    public ContactStorage() {
+        this.orderedContacts = new ArrayList<>();
+        this.contacts = new HashSet<>();
+        this.groupedContacts = new HashMap<>();
+    }
+}
