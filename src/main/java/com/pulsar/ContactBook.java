f09c508f436626391b53d395aa471083fc272ed3
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-11 21:27:14 +0300

    add: create the Command enum in the ContactBook class

--- src/main/java/com/pulsar/ContactBook.java
+++ src/main/java/com/pulsar/ContactBook.java
@@ -1,5 +1,6 @@
 package com.pulsar;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ContactBook {
@@ -15,5 +16,33 @@ public class ContactBook {
     public ContactBook(ContactStorage contactStorage) {
         this.contactStorage = contactStorage;
         this.terminal = new Scanner(System.in);
+    }
+
+    private enum Command {
+        ADD_CONTACT("1"),
+        DELETE_CONTACT("2"),
+        VIEW_ALL_CONTACTS("3"),
+        FIND_CONTACTS("4"),
+        FIND_CONTACT("5"),
+        PRINT_CONTACTS_BY_GROUP("6"),
+        EXIT("0");
+
+        private final String value;
+
+        Command(String value) {
+            this.value = value;
+        }
+
+        public static Command get(String command) {
+            return Arrays.stream(values())
+                    .filter(it -> it.value.equals(command))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Некорректный ввод!"));
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
     }
 }
