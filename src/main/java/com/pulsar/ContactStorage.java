00bd46b3d88895959d9bc160fead7d8afb3d3de7
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-11 15:34:44 +0300

    add: implement the add() method in the ContactStorage class

--- src/main/java/com/pulsar/ContactStorage.java
+++ src/main/java/com/pulsar/ContactStorage.java
@@ -1,6 +1,7 @@
 package com.pulsar;
 
 import com.pulsar.model.Contact;
+import com.pulsar.util.Printer;
 
 import java.util.*;
 
@@ -8,12 +9,23 @@ public class ContactStorage {
 
     private final List<Contact> orderedContacts;
     private final Set<Contact> contacts;
-    private final Map<String, Contact> groupedContacts;
+    private final Map<String, List<Contact>> groupedContacts;
 
     public ContactStorage() {
         this.orderedContacts = new ArrayList<>();
         this.contacts = new HashSet<>();
         this.groupedContacts = new HashMap<>();
+    }
+
+    public void add(Contact contact) {
+        if (contains(contact)) {
+            Printer.displayError("Данный контакт уже существует");
+        } else {
+            orderedContacts.add(contact);
+            contacts.add(contact);
+            groupedContacts.computeIfAbsent(contact.getGroup(), k -> new ArrayList<>())
+                    .add(contact);
+        }
     }
 
     public boolean contains(Contact contact) {
